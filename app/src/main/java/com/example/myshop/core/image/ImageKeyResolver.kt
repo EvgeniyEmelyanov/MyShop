502ba203f44a3a89e189ec21a88578dee365b063
Evgeniy Emelyanov <jazenja4@gmal.com>
2026-03-24 13:55:30 +0300

    Implemented Clean Architecture in the Explore. Restructure directories.

---
+++ app/src/main/java/com/example/myshop/core/image/ImageKeyResolver.kt
@@ -0,0 +1,17 @@
+package com.example.myshop.core.ui.image
+
+import androidx.annotation.DrawableRes
+import com.example.myshop.R
+
+object ImageKeyResolver { // временно делаем обджектом пока не перевели все на рхитектуру.
+
+    @DrawableRes
+    fun resolve(imageKey: String): Int =
+        when (imageKey) {
+            "apple_picture" -> R.drawable.apple_picture
+            "banana_picture" -> R.drawable.banana_picture
+            "pepper_picture" -> R.drawable.pepper_picture
+            // добавляй дальше…
+            else -> R.drawable.apple_picture // сделай плейсхолдер
+        }
+}
